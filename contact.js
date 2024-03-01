fbb30425889476079af01ed082428a5e39c1a87f
Samuel Chinecherem <samdesley@gmail.com>
2024-03-01 16:51:27 +0000

    first commit

---
+++ contact.js
@@ -0,0 +1,19 @@
+
+
+ form.addEventListener('submit', (e) => {
+     e.preventDefault();
+    // checkInput();
+    submit();
+ });
+
+
+// console.log(checkInput);
+
+
+function submit(){
+   alert('Thanks for your messages')
+}
+
+submit()
+
+
---
+++ thanks.js
@@ -0,0 +1,5 @@
+const ggamePlayers= [player1, player2]
+
+
+
+alert('Thanks for your Response!')
